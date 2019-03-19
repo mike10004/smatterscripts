cb28e606cf0798809b5f380917bba96662bb69d2
Mike Chaberski <mchaberski@novetta.com>
2019-03-19 11:47:11 -0400

    implement csv2sortable

--- shelltools/csv2sortable.py
+++ shelltools/csv2sortable.py
@@ -2,5 +2,13 @@
 
 """Program that reads a CSV file and prints text suitable for GNU `sort`."""
 
+import sys
+import csv
+
 def main():
-    return 1
+    outdelim = "\t"
+    indelim = ","
+    ofile = sys.stdout
+    for row in csv.reader(sys.stdin, delimiter=indelim):
+        print(outdelim.join(row), file=ofile)
+    return 0
