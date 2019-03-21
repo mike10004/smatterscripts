30050553765df8433ce2287c0fab3819cdca9ce4
Mike Chaberski <mchaberski@novetta.com>
2019-03-21 15:50:27 -0400

    ressample: support preserve order

--- shelltools/ressample.py
+++ shelltools/ressample.py
@@ -11,83 +11,65 @@ from __future__ import print_function
 from _common import StreamContext
 from argparse import ArgumentParser
 import random
-import sys
-from typing import TypeVar, Iterator, List
+import errno
+import logging
+from operator import itemgetter
+from typing import Iterator, List
 
-T = TypeVar('T')
 
+_log = logging.getLogger(__name__)
+
+
+# noinspection PyMethodMayBeStatic
 class ReservoirSampler(object):
 
-    conserve = False
     preserve_order = False
 
     def __init__(self, rng: random.Random):
         self.rng = rng
 
-    def _replace_item(self, item, n, s, subset):
-        if self.conserve:
-            subset[s] = n
-        else:
-            subset[s] = item
+    def _replace_item(self, item, s, subset):
+        subset[s] = item
 
-    def _append_item(self, item, n, subset):
-        if self.conserve:
-            subset.append(n)
-        else:
-            subset.append(item)
+    def _append_item(self, item, subset):
+        subset.append(item)
 
     def collect(self, iterator: Iterator, k: int) -> List:
         result = []
-        n = 0
-        for item in iterator:
-            n += 1
+        for item in enumerate(iterator):
+            n = item[0]
             if len(result) < k:
-                self._append_item(item, n, result)
+                self._append_item(item, result)
             else:
-                s = int(self.rng.random() * n)
+                s = int(self.rng.random() * (n + 1))
                 if s < k:
-                    self._replace_item(item, n, s, result)
-        return result
+                    self._replace_item(item, s, result)
+        if self.preserve_order:
+            result.sort(key=itemgetter(0))
+        return list(map(itemgetter(1), result))
 
-
-def get_reservoir_sample(iterator: Iterator, k, args) -> List:
-    sampler = ReservoirSampler(random.SystemRandom())
-    sampler.conserve = args.conserve
-    return sampler.collect(iterator, k)
-
-def _print_lines(ifile, subset, args):
-    subset.sort(reverse=True)
-    n = 0
-    k = len(subset)
-    for line in ifile:
-        n += 1
-        if subset[k - 1] == n:
-            print(line, end="")
-            del subset[k - 1]
-            k = len(subset)
 
 def main():
-    parser = ArgumentParser()
+    parser = ArgumentParser(description="Samples lines from an input stream.", epilog="Bear in mind that the sample is collected in memory before being printed. Exits clean unless I/O error occurs or input is too small.")
     parser.add_argument("k", type=int, metavar="K", help="sample size")
-    parser.add_argument("inputfile", nargs='?', 
-            help="input file; if absent or - then uses stdin")
-    parser.add_argument("--conserve", action="store_true", 
-            help="conserve space at expense of time; only retains indices"
-            +" of sampled items and iterates over input again to print")
+    parser.add_argument("inputfile", nargs='?', help="input file; if absent or - then uses stdin")
+    parser.add_argument("-p", "--preserve-order", action="store_true", help="preserve order from stream")
     args = parser.parse_args()
-    if args.conserve and args.inputfile is None:
-        print("ressample: can't use stdin input with --conserve", file=sys.stderr)
-        return 1
+    sampler = ReservoirSampler(random.SystemRandom())
+    for attr in ('preserve_order',):
+        sampler.__setattr__(attr, args.__dict__[attr])
     with StreamContext(args.inputfile, 'r') as iterator:
-        sample = get_reservoir_sample(iterator, args.k, args)
-    if args.conserve:
-        with open(args.inputfile, 'r') as ifile:
-            _print_lines(ifile, sample, args)
-    else:
+        sample = sampler.collect(iterator, args.k)
+    try:
         for s in sample:
             print(s, end="")
+    except IOError as e:
+        if errno.EPIPE == e.errno:
+             _log.info("broken pipe; not all items from sample printed")
+        else:
+            raise
     if len(sample) < args.k:
-        print("ressample: not enough items in input; sample contains only", len(sample), file=sys.stderr)
+        _log.warning("not enough items in input; sample contains only %s", len(sample))
         return 2
     return 0
 
--- tests/test_ressample.py
+++ tests/test_ressample.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
-from shelltools import ressample
-from shelltools.ressample import ReservoirSampler
-from unittest import TestCase
-from argparse import Namespace
 import random
+from unittest import TestCase
 
+from shelltools.ressample import ReservoirSampler
 
 _TEST_SEED = 0xDeadBeef
-
-
-class ModuleTest(TestCase):
-
-    def test_partial(self):
-        n, k = 100, 10
-        sample = ressample.get_reservoir_sample(range(n), k, Namespace(conserve=False))
-        self.assertEqual(k, len(sample))
-        self.assertEqual(len(sample), len(set(sample)))
-        container = set(range(n))
-        for item in sample:
-            self.assertIn(item, container)
 
 
 class ReservoirSamplerTest(TestCase):
@@ -35,11 +21,12 @@ class ReservoirSamplerTest(TestCase):
         self.assertListEqual(self.expected10, sample)
 
     def test_preserve_order(self):
-        n, k = 100, 10
+        k = 5
+        items = "zyxwvutsrqponmlkjihgfedcba"
         sampler = ReservoirSampler(self.rng)
         sampler.preserve_order = True
-        sample = sampler.collect(range(n), k)
-        self.assertListEqual(sorted(self.expected10), sample)
+        sample = sampler.collect(items.__iter__(), k)
+        self.assertListEqual(sorted(['a', 'c', 'u', 'x', 'y'], reverse=True), sample)
 
     def test_samesize(self):
         n = 25
