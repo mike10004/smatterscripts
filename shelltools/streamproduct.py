cb7fe613d94a5d9391ebf910754cf0c245399f71
Mike Chaberski <mchaberski@novetta.com>
2019-03-22 17:17:29 -0400

    improve streamproduct cli

--- shelltools/streamproduct.py
+++ shelltools/streamproduct.py
@@ -5,22 +5,49 @@ import sys
 from argparse import ArgumentParser
 import itertools
 from shelltools.ressample import ReservoirSampler
+from typing import Sequence
 
-def generate(input_args):
-    iterables = []
-    for input_arg in input_args:
-        with StreamContext(input_arg, 'r') as ifile:
-            iterables.append([line.rstrip("\r\n") for line in ifile])
-    return itertools.product(*iterables)
+def _has_dupes(items: Sequence):
+    if len(items) <= 1:
+        return False
+    if len(items) == 2:
+        return items[0] == items[1]
+    # would len(set(items)) < len(items) be faster?
+    for i in range(len(items)):
+        for j in range(i + 1, len(items)):
+            if items[i] == items[j]:
+                return True
+    return False
+
+class Generator(object):
+
+    avoid_dupes = False
+
+    # noinspection PyMethodMayBeStatic
+    def _uniques(self, iterator):
+        for combo in iterator:
+            if not _has_dupes(combo):
+                yield combo
+
+    def generate(self, input_args):
+        iterables = []
+        for input_arg in input_args:
+            with StreamContext(input_arg, 'r') as ifile:
+                iterables.append([line.rstrip("\r\n") for line in ifile])
+        all_combos = itertools.product(*iterables)
+        if self.avoid_dupes:
+            return self._uniques(all_combos)
+        else:
+            return all_combos
 
 def render(selection, delimiter, ofile=sys.stdout):
     print(*selection, sep=delimiter, file=ofile)
 
 def main():
-    parser = ArgumentParser(epilog="Note that all input content is stored in memory.")
+    parser = ArgumentParser(description="Print combinations of items from multiple streams.", epilog="Note that all input content is stored in memory.")
     parser.add_argument("input", nargs='+', metavar="FILE", help="multiple files from which product will be printed")
     parser.add_argument("-k", "--sample", type=int, metavar="K", help="sample size")
-    parser.add_argument("--delimiter", default=' ')
+    parser.add_argument("-d", "--delimiter", default=' ', metavar="STR", help="set delimiter between items on each line")
 
     args = parser.parse_args()
     if len(args.input) == 1:
@@ -30,11 +57,12 @@ def main():
     if len(list(filter(lambda x: x == '-' or x == '/dev/stdin', args.input))) > 1:
         print("streamproduct: at most one argument may specify standard input", file=sys.stderr)
         return 1
+    g = Generator()
     if args.sample is not None:
         sampler = ReservoirSampler()
-        combos = sampler.collect(generate(args.input), args.sample)
+        combos = sampler.collect(g.generate(args.input), args.sample)
     else:
-        combos = generate(args.input)
+        combos = g.generate(args.input)
     for selection in combos:
         render(selection, args.delimiter)
     return 0
--- tests/test_streamproduct.py
+++ tests/test_streamproduct.py
@@ -1,22 +1,72 @@
 import os
 import os.path
 from shelltools import streamproduct
+from shelltools.streamproduct import Generator
 from unittest import TestCase
 import tempfile
 import itertools
+import logging
+
+
+_log = logging.getLogger(__name__)
+
+
+def write_items(iterator, pathname):
+    with open(pathname, 'w') as ofile:
+        for item in iterator:
+            print(item, file=ofile)
+    return pathname
 
 
 class ModuleTest(TestCase):
 
-    def test_generate(self):
-        c1, c2 = "abc", "de"
+    def test__has_dupes(self):
+        test_cases = [
+            ("", False),
+            ("a", False),
+            ("aa", True),
+            ("ab", False),
+            ("abc", False),
+            ("aba", True),
+            ("abcd", False),
+        ]
+        for seq, expected in test_cases:
+            seq = tuple(seq)
+            with self.subTest():
+                actual = streamproduct._has_dupes(seq)
+                self.assertEqual(expected, actual, str(seq))
+
+
+class GeneratorTest(TestCase):
+
+    def test_generate_twofiles(self):
+        self._generate("abc", "de")
+
+    def test_generate_twofiles_avoid_dupes(self):
+        self._generate("abc", "dbe", avoid_dupes=True)
+
+    def test_generate_samefile(self):
+        chars = "abcd"
         with tempfile.TemporaryDirectory() as tempdir:
-            p1 = os.path.join(tempdir, "f1.txt")
-            p2 = os.path.join(tempdir, "f2.txt")
-            with open(p1, 'w') as ofile:
-                print(os.linesep.join(c1), end="", file=ofile)
-            with open(p2, 'w') as ofile:
-                print(os.linesep.join(c2), file=ofile)
-            generated = list(streamproduct.generate([p1, p2]))
-        expected = set(itertools.product(c1, c2))
+            pathname = os.path.join(tempdir, "file.txt")
+            write_items(chars, pathname)
+            self._generate_from_files([chars, chars], [pathname, pathname])
+
+    def _generate(self, *iterables, avoid_dupes=False):
+        with tempfile.TemporaryDirectory() as tempdir:
+            pathnames = []
+            for i, iterable in enumerate(iterables):
+                pathname = os.path.join(tempdir, f"file{i}.txt")
+                write_items(iterable, pathname)
+                pathnames.append(pathname)
+                _log.debug("wrote file %s", pathname)
+            self._generate_from_files(iterables, pathnames, avoid_dupes)
+
+    def _generate_from_files(self, iterables, pathnames, avoid_dupes=False):
+        g = Generator()
+        g.avoid_dupes = avoid_dupes
+        generated = list(g.generate(pathnames))
+        expected = set(itertools.product(*iterables))
+        if avoid_dupes:
+            expected = set(filter(lambda combo: len(combo) == len(set(combo)), expected))
         self.assertSetEqual(expected, set(generated))
